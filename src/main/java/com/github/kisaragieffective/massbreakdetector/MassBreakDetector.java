871f9db15a809234a8e2d822dd30e8986e10c744
Kisaragi Marine <kisaragi.effective@gmail.com>
2023-03-30 21:21:23 +0900

    change main class name

--- src/main/java/com/github/kisaragieffective/massbreakdetector/MassBreakDetector.java
+++ src/main/java/com/github/kisaragieffective/massbreakdetector/MassBreakDetector.java
@@ -2,7 +2,7 @@ package com.github.kisaragieffective.massbreakdetector;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Massbreakdetector extends JavaPlugin {
+public final class MassBreakDetector extends JavaPlugin {
 
     @Override
     public void onEnable() {
